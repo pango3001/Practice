cd792ccf2279c858b4083124fe44e94972bbec72
Jesse.ms <pango3001@gmail.com>
2019-02-27 12:03:20 -0600

    Made an example

---
+++ classes/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Pickle{
+    private:
+        int x;
+        int y;
+    public:
+        int z;
+        void setValues(){
+            cout << "First Number: ";
+            cin >> x;
+            cout << "second Number: ";
+            cin >> y;
+
+
+        }
+        int product(){
+            int product = x * y;
+            return product;
+        }
+
+
+};
+
+int main(){
+    Pickle one;
+
+    one.setValues();
+    int x = one.product();
+
+    cout << x << endl;
+
+    return 0;
+}
